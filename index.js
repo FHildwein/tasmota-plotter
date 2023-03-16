9aa8312472699d95b3d0e2fa5b055bfdfaf9ba3e
Florian <Florian.Hildwein@protonmail.com>
2023-03-16 14:16:32 +0100

    update, cleaned data files and new daily powerusage feature

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const http = require('http');
 const PORT = 3000;
 
 var currentTime = '';
-var socket1 = ['t1-desk   ', 'http://192.168.178.101/cm?cmnd=status%208', './data/t1-desk.csv', '0'];
-var socket2 = ['t2-kitchen', 'http://192.168.178.102/cm?cmnd=status%208', './data/t2-kitchen.csv', '0'];
-var socket3 = ['t3-room   ', 'http://192.168.178.103/cm?cmnd=status%208', './data/t3-room.csv', '0'];
-var socket4 = ['t4-attic  ', 'http://192.168.178.104/cm?cmnd=status%208', './data/t4-attic.csv', '0'];
+var socket1 = ['t1-desk   ', 'http://192.168.178.101/cm?cmnd=status%208', './data/t1-desk.csv', './data/t1-desk-daily.csv', '0'];
+var socket2 = ['t2-kitchen', 'http://192.168.178.102/cm?cmnd=status%208', './data/t2-kitchen.csv', './data/t2-kitchen-daily.csv', '0'];
+var socket3 = ['t3-room   ', 'http://192.168.178.103/cm?cmnd=status%208', './data/t3-room.csv', './data/t3-room-daily.csv', '0'];
+var socket4 = ['t4-attic  ', 'http://192.168.178.104/cm?cmnd=status%208', './data/t4-attic.csv', './data/t4-attic-daily.csv', '0'];
 
 setInterval(main, 20000);
 
@@ -59,15 +59,20 @@ async function writeDataPoint(socket) {
     const data = await readData(socket[1]);
 
     var power = data.StatusSNS.ENERGY.Power;
-    var time = data.StatusSNS.Time;
+    var timeAndDate = data.StatusSNS.Time;
 
-    currentTime = time;
-    time = time.slice(5);
-    time = time.replace('T', '-')
+    timeAndDate = timeAndDate.replace('T', '-')
 
-    power = (power + parseInt(socket[3]) * 3 ) / 4;
-    socket[3] = String(power);
-    
+    currentTime = timeAndDate;
+    time = timeAndDate.slice(5);
+    date = timeAndDate.slice(0, 10);
+
+    if (date != socket[4]) {
+        var powerDaily = data.StatusSNS.ENERGY.Yesterday;
+        fs.appendFileSync(socket[3], date + ',' + String(powerDaily) + '\n');
+        socket[4] = date;
+    }
+
     fs.appendFileSync(socket[2], time + ',' + String(power) + '\n');
     console.log(socket[0] + ' : ' + power);
     return;
--- plot.py
+++ plot.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('data/t1-desk.csv')
+data = pd.read_csv('data/t4-attic.csv')
 
 fig = px.line(data, x = 'Time', y = 'Power', title='Power usage')
 fig.show()
